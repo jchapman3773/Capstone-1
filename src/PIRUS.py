88656642d25ac62e6eeea69a4b6b1304bfba8b29
Julia <juliachapman3773@gmail.com>
2018-10-10 11:52:06 -0600

    Make coefficient descents

--- src/PIRUS.py
+++ src/PIRUS.py
@@ -3,9 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils import XyScaler
 import statsmodels.api as sm
-from sklearn.linear_model import LinearRegression, LassoCV
+from sklearn.linear_model import LinearRegression, LassoCV, ElasticNetCV
 from sklearn.model_selection import train_test_split, KFold
-from sklearn.preprocessing import StandardScaler
 from statsmodels.stats import outliers_influence, diagnostic
 from sklearn.base import clone
 import matplotlib as mpl
@@ -28,41 +27,79 @@ class Data:
 
     def __init__(self,data,predict):
         self.df = data
+        self.predict = predict
+        self.X = None
+        self.y = None
+        self.X_scale = None
+        self.y_scale = None
         self.X_train = None
         self.X_test = None
         self.y_train = None
         self.y_test = None
-        self.predict = predict
+        self.clean_data = None
 
-    def clean_data(self):
+    def create_clean_data(self):
         columns = list(self.df.loc[:,'Broad_Ethnicity':'Aspirations'].columns)
         columns += ['Violent']
         columns.remove('Age_Child')
         self.df.Gender.replace({1:0,2:1})
         self.df.Language_English.replace(-88,1)
         self.df[['Education_Change','Change_Performance','Work_History','Social_Stratum_Adulthood']].replace(-88,'NaN')
-        clean_df = self.df[columns].select_dtypes(exclude='object').fillna(df.mean())
-        return clean_df
+        self.clean_data = self.df[columns].select_dtypes(exclude='object').fillna(df.mean())
+        self.y = self.clean_data[self.predict]
+        self.X = self.clean_data.drop(self.predict,axis=1)
 
-    def data_clean_split(self):
-        clean_df = self.clean_data()
-        y = clean_df[self.predict]
-        X = clean_df.drop(self.predict,axis=1)
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.1)
-        return
+    def fix_imbalance(self):
+        pass
+
+    def scale_data(self):
+        X_scale,y_scale = self.scaler.fit_transform(self.X,self.y)
+        self.X_scale = pd.DataFrame(data=X_scale,columns=self.X.columns,index=self.X.index)
+        self.y_scale = pd.Series(data=y_scale)
+
+    def split_data(self,split=0.2):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X_scale, self.y_scale, test_size=split)
+
+    def prep_data(self):
+        self.create_clean_data()
+        self.fix_imbalance()
+        self.scale_data()
+        self.split_data()
 
 class Model(Data):
 
-    def __init__(self,data,model,predict):
-        self.model = model
-        self.trained_model = None
-        self.scaler = XyScaler()
+    def __init__(self,data,model,predict,name):
         super().__init__(data,predict)
+        self.model = model
+        self.name = name
+        self.alphas = None
+        self.log_alphas = None
+        self.scaler = XyScaler()
+
+    def get_alphas(self):
+        self.alphas = self.model.alphas_
+        self.log_alphas = np.log10(self.alphas)
+
+    def fit_model(self):
+        self.model.fit(self.X_train,self.y_train)
+
+    def plot_coef_log_alphas(self):
+        coeffs = self.model.path(self.X_train,self.y_train)[1]
+        plt.plot(self.log_alphas,coeffs.T)
+        plt.title(f'Coefficient Descent of {self.name}')
+        plt.xlabel(r'log($\alpha$)')
+        plt.ylabel('Coefficients')
+        plt.savefig(f'Coefficient Descent of {self.name}.png')
+
+    def plot_mse(self):
+        pass
+
+    def plot_ROC(self):
+        pass
 
     def get_vifs(self):
         for idx, col in enumerate(self.X_train.columns):
             print(f"{col}: {outliers_influence.variance_inflation_factor(self.X_train.values,idx)}")
-        return
 
     def get_goldsfeltquandt(self):
         print(diagnostic.het_goldfeldquandt(self.trained_model.resid, self.trained_model.model.exog))
@@ -71,9 +108,14 @@ class Model(Data):
         linear_model = sm.OLS(self.y_train, self.X_train).fit()
         print(linear_model.summary2())
 
+    def clean_split_fit(self):
+        self.prep_data()
+        self.fit_model()
+        self.get_alphas()
 
 if __name__ == '__main__':
-    PIRUS = Model(df, LassoCV(), 'Violent')
-    PIRUS.data_clean_split()
-    PIRUS.get_vifs()
-    PIRUS.get_linear_summary()
+    PIRUS = Model(df, LassoCV(), 'Violent','Lasso')
+    PIRUS.clean_split_fit()
+    PIRUS.plot_coef_log_alphas()
+    # PIRUS.get_vifs()
+    plt.show()
--- src/utils.py
+++ src/utils.py
@@ -8,20 +8,23 @@ class XyScaler(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.X_scaler = StandardScaler()
         self.y_scaler = StandardScaler()
-        
+
     def fit(self, X, y, *args, **kwargs):
         """Fit the scaler to data and a target vector."""
         self.X_scaler.fit(X)
-        self.y_scaler.fit(y.reshape(-1, 1))
+        self.y_scaler.fit(y.values.reshape(-1, 1))
         return self
-    
+
     def transform(self, X, y, *args, **kwargs):
         """Transform a new set of data and target vector."""
         return (self.X_scaler.transform(X),
-                self.y_scaler.transform(y.reshape(-1, 1)).flatten())
+                self.y_scaler.transform(y.values.reshape(-1, 1)).flatten())
 
     def inverse_transform(self, X, y, *args, **kwargs):
         """Tranform from a scaled representation back to the original scale."""
         return (self.X_scaler.inverse_transform(X),
-                self.y_scaler.inverse_transform(y.reshape(-1, 1)).flatten())
+                self.y_scaler.inverse_transform(y.values.reshape(-1, 1)).flatten())
 
+    def fit_transform(self, X, y, *args, **kwargs):
+        self.fit(X, y)
+        return self.transform(X, y)
